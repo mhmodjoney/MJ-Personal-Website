7f8e4c2e187fd30e6a7c6dd5db2a87bb9ae0d136
Mahmoud Jouny <mahmoudjouny99@gmail.com>
2023-10-30 15:40:06 +0300

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
         <li><a href="https://github.com/mhmodjoney"><i class="fa fa-github"></i></a></li>
         <li><a href="https://www.facebook.com/profile.php?id=100010003903953"><i class="fa fa-facebook"></i></a></li>
         <li><a href="https://www.linkedin.com/in/mahmoud-jouny-5611b4248/"><i class="fa fa-linkedin"></i></a></li>
-         <li><a href="https://wa.me/963938261614"><i class="fa fa-whatsapp"></i></a></li>
+         <li><a href="https://wa.me/971567125726"><i class="fa fa-whatsapp"></i></a></li>
     </ul>
       <span>
                     <a href="mailto:mahmoudjouny99@gmail.com">{"mahmoudjouny99@gmail.com"}</a>
